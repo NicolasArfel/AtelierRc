a570b5e4bd89bbc51b83adb29ad4af8f5aa5b0c0
Nicolas Arfel <nicolasarfel@gmail.com>
2022-06-09 18:09:04 +0200

    Banner.css modified

--- client/src/components/Banner/Banner.js
+++ client/src/components/Banner/Banner.js
@@ -1,8 +1,10 @@
+import './Banner.css'
+
 const Banner = ( {title, description}) => {
     return (
-        <div className="about__banner ">
-                <h1 className="about__banner-title" > {title} </h1>
-                <p className="about__banner-description" > {description} </p>
+        <div className="banner ">
+                <h1 className="banner__title" > {title} </h1>
+                <p className="banner__description" > {description} </p>
         </div>
     )
 }
