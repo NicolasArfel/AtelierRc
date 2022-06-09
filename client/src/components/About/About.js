a8e7415494a91d3b145a540c978168b5b90a5f56
Nicolas Arfel <nicolasarfel@gmail.com>
2022-06-09 16:03:53 +0200

    first step about-page

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,1 +1,25 @@
-// toto
+import './About.css'
+
+const AboutImg = '../../images/about.jpeg'
+
+
+const About = () => {
+    return (
+        <div className="container">
+        <div className="row">
+            <img className="about_img col s6" src={AboutImg} alt="design of a living room" />
+            <p className="col s6" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem corporis eum minus, doloremque soluta ipsa cum temporibus ex velit similique consequatur quasi tempora quas fuga officia fugiat ut? Odit, qui.
+            Enim accusantium tempore sequi perspiciatis? Reiciendis culpa animi perspiciatis quos asperiores pariatur at unde exercitationem consequuntur aliquid, doloribus delectus tenetur quo id sapiente quasi veritatis? Tempora quae explicabo facere doloribus!
+            Facilis alias cumque mollitia laudantium, deleniti iste enim repellat, amet repellendus consectetur doloremque expedita at libero necessitatibus aliquam blanditiis voluptatum natus neque soluta possimus perferendis hic! Obcaecati iure quasi ratione.
+            Ut expedita ea asperiores autem iure minima, aut ullam ducimus. Nemo dignissimos modi aut iste, ullam laudantium nam cumque vero, quibusdam labore sed molestias quisquam totam a porro? Recusandae, ducimus?
+            Animi dolor placeat aperiam laborum, a, magni veniam voluptate neque doloribus impedit deleniti deserunt accusamus at consequuntur? Maiores ipsum, aliquam ullam, ex asperiores in harum totam deleniti, maxime itaque officiis.
+            Consequatur blanditiis, quis veniam cupiditate quibusdam natus repellendus, quae modi nulla eveniet porro ullam? Explicabo suscipit eveniet distinctio dolorem minus harum, dolorum fuga provident corrupti obcaecati. Consequuntur itaque placeat qui!
+            Dicta perspiciatis eaque doloribus error illo accusamus fugit est, voluptatibus quasi eligendi fugiat dolorem. Assumenda totam error fugit nulla sint illum, deleniti nostrum aut. Eveniet exercitationem enim voluptate quaerat doloribus?
+            Sunt, recusandae odit, sit quae possimus quia repellendus aliquam iste nam accusamus rerum illum, voluptatem harum ducimus dolore reiciendis excepturi provident accusantium soluta impedit? Obcaecati voluptates quis hic perferendis iusto.</p>
+        </div>
+
+        </div>
+    )
+};
+
+export default About;
--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,11 +1,13 @@
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import './App.css';
+import About from '../About/About';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <About/>
       <Footer />
     </div>
   );
