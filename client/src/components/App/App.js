9780af59415f1762f5c004119f017d101ff92218
Nicolas Arfel <nicolasarfel@gmail.com>
2022-06-09 16:04:35 +0200

    first step about-page without App link

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,13 +1,12 @@
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import './App.css';
-import About from '../About/About';
+
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-      <About/>
       <Footer />
     </div>
   );
