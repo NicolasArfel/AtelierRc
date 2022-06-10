0664d3b81734cf25aff6c2e4d622c40abdbdbf21
Nicolas Arfel <nicolasarfel@gmail.com>
2022-06-10 17:42:18 +0200

    resize of the project images + responsive

--- client/src/components/Projets/Projets.js
+++ client/src/components/Projets/Projets.js
@@ -20,54 +20,40 @@ const Projets = () => {
         <main className="container" >
             <Banner title={title} description={description} />
             <div className="row project__section">
-                <article className="col s12 m6 l3 project__article card">
-                    <Link to="/projet/:slug" >
-                        <div className="card-image card__image z-depth-2">
-                            <img className="responsive-img" alt={projet4} src={projet4} />
-                            <span className="card-title black-text">Card Title</span>
-                        </div>
-                    </Link>
-                </article>
-                <article className="col s12 m6 l3 project__article card">
-                    <Link to="/projet/:slug" >
-                        <div className="card-image card__image z-depth-2">
-                            <img className="responsive-img" alt={projet2} src={projet2} />
-                            <span className="card-title black-text">Card Title</span>
-                        </div>
-                    </Link>
-                </article>
-                <article className="col s12 m6 l3 project__article card">
-                    <Link to="/projet/:slug" >
-                        <div className="card-image card__image z-depth-2">
-                            <img className="responsive-img" alt={projet3} src={projet3} />
-                            <span className="card-title black-text">Card Title</span>
-                        </div>
-                    </Link>
-                </article>
-                <article className="col s12 m6 l3 project__article card">
-                    <Link to="/projet/:slug" >
-                        <div className="card-image card__image z-depth-2">
-                            <img className="responsive-img" alt={projet5} src={projet5} />
-                            <span className="card-title black-text">Card Title</span>
-                        </div>
-                    </Link>
-                </article>
-                <article className="col s12 m6 l3 project__article card">
-                    <Link to="/projet/:slug" >
-                        <div className="card-image card__image z-depth-2">
-                            <img className="responsive-img" alt={projet1} src={projet1} />
-                            <span className="card-title black-text">Card Title</span>
-                        </div>
-                    </Link>
-                </article>
-                <article className="col s12 m6 l3 project__article card" >
-                    <Link to="/projet/:slug" >
-                        <div className="card-image card__image z-depth-2">
-                            <img className="responsive-img" alt={projet6} src={projet6} />
-                            <span className="card-title black-text">Card Title</span>
-                        </div>
-                    </Link>
-                </article>
+                <Link to="/projet/:slug" >
+                    <div className="card-image card__image ">
+                        <img className="responsive-img z-depth-2" alt={projet4} src={projet4} />
+                    </div>
+                </Link>
+                <Link to="/projet/:slug" >
+                    <div className="card-image card__image ">
+                        <img className="responsive-img z-depth-2" alt={projet2} src={projet2} />
+                    </div>
+                </Link>
+                <Link to="/projet/:slug" >
+                    <div className="card-image card__image ">
+                        <img className="responsive-img z-depth-2" alt={projet3} src={projet3} />
+
+                    </div>
+                </Link>
+                <Link to="/projet/:slug" >
+                    <div className="card-image card__image ">
+                        <img className="responsive-img z-depth-2" alt={projet5} src={projet5} />
+
+                    </div>
+                </Link>
+                <Link to="/projet/:slug" >
+                    <div className="card-image card__image ">
+                        <img className="responsive-img z-depth-2" alt={projet1} src={projet1} />
+
+                    </div>
+                </Link>
+                <Link to="/projet/:slug" >
+                    <div className="card-image card__image ">
+                        <img className="responsive-img z-depth-2" alt={projet6} src={projet6} />
+
+                    </div>
+                </Link>
             </div>
         </main>
     )
