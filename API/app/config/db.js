8c104e2458c079809d71a103a1cdbb73588fc055
VeroniqueMoine <moine.veronique@gmail.com>
2022-06-14 14:00:26 +0200

    project and furniture routes functionnal and photos routes test deleted

--- API/app/config/db.js
+++ API/app/config/db.js
@@ -1,28 +1,20 @@
-const { Client } = require('pg');
-const client = new Client(process.env.DATABASE_URL);
-client.connect();
-
-module.exports = client;
-
-
-
-//  const debug = require('debug')('SQL:log');
-//  const { Pool } = require('pg');
+const debug = require('debug')('SQL:log');
+const { Pool } = require('pg');
  
-//  const dbConfig = {
-//      connectionString: process.env.DATABASE_URL,
-//  };
+ const dbConfig = {
+     connectionString: process.env.DATABASE_URL,
+ };
  
-//  if (process.env.NODE_ENV === 'production') {
-//      dbConfig.ssl = { rejectUnauthorized: false };
-//  }
+ if (process.env.NODE_ENV === 'production') {
+     dbConfig.ssl = { rejectUnauthorized: false };
+ }
  
-//  const pool = new Pool(dbConfig);
+ const pool = new Pool(dbConfig);
  
-//  module.exports = {
-//      originalClient: pool,
-//      async query(...params) {
-//          debug(...params);
-//          return this.originalClient.query(...params);
-//      },
-//  };
+ module.exports = {
+     originalClient: pool,
+     async query(...params) {
+         debug(...params);
+         return this.originalClient.query(...params);
+     },
+ };
--- API/app/router.js
+++ API/app/router.js
@@ -4,7 +4,6 @@ const express = require('express');
 // importer les controllers
 const projectController = require('./controllers/api/projectController');
 const furnitureController = require('./controllers/api/furnitureController');
-const projectPhotoController = require('./controllers/api/projectPhotoController');
 
 const router = express.Router();
 
@@ -15,10 +14,6 @@ router.get('/', (req, res) => {
 /** Projects */
 router.get('/api/projects', projectController.getAllProjects);
 router.get('/api/project/:id', projectController.getOne);
-
-/** Photos */
-router.get('/api/photos/', projectPhotoController.getAllPhotos);
-router.get('/api/photo/:id', projectPhotoController.getOne);
 
 /* Furnitures */
 router.get('/api/furnitures', furnitureController.getAllFurnitures);
