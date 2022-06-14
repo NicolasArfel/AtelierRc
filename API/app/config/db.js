d9e39ff080116f47e3688a269169adc97e36b795
VeroniqueMoine <moine.veronique@gmail.com>
2022-06-14 10:17:27 +0200

    get all the photos for one project and the cover photo for all the projects

--- API/app/config/db.js
+++ API/app/config/db.js
@@ -3,3 +3,26 @@ const client = new Client(process.env.DATABASE_URL);
 client.connect();
 
 module.exports = client;
+
+
+
+//  const debug = require('debug')('SQL:log');
+//  const { Pool } = require('pg');
+ 
+//  const dbConfig = {
+//      connectionString: process.env.DATABASE_URL,
+//  };
+ 
+//  if (process.env.NODE_ENV === 'production') {
+//      dbConfig.ssl = { rejectUnauthorized: false };
+//  }
+ 
+//  const pool = new Pool(dbConfig);
+ 
+//  module.exports = {
+//      originalClient: pool,
+//      async query(...params) {
+//          debug(...params);
+//          return this.originalClient.query(...params);
+//      },
+//  };
