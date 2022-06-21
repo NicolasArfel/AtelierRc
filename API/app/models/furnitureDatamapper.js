98793591d017cf476edcb875c6d6536dcdc2d542
Alix <alemappian@hotmail.fr>
2022-06-21 11:04:14 +0200

    update routes GET furniture

--- API/app/models/furnitureDatamapper.js
+++ API/app/models/furnitureDatamapper.js
@@ -3,13 +3,13 @@ const client = require('../config/db');
 const furnitureDatamapper = {
     
     async findAll() {
-            const result = await client.query('SELECT * FROM furniture');
+            const result = await client.query('SELECT furniture.name AS furniture_name, * FROM "furniture" INNER JOIN furniture_photo ON furniture_photo.furniture_id = furniture.id WHERE cover_photo = true');
             return result.rows;
     },
 
     async findByPk(id) {
         const preparedQuery = {
-            text: `SELECT * FROM "furniture" WHERE "id" = $1`,
+            text: 'SELECT furniture.name AS furniture_name, * FROM "furniture" INNER JOIN furniture_photo ON furniture_photo.furniture_id = furniture.id WHERE furniture_id = $1',
             values: [id]
         }
         const result = await client.query(preparedQuery);
