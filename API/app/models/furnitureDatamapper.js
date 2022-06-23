976d1e25fc9c6f881341e0d861590239c6a651c6
anthonys13 <salleanthony3@gmail.com>
2022-06-23 23:17:28 +0200

    merge front-backfurniture

--- API/app/models/furnitureDatamapper.js
+++ API/app/models/furnitureDatamapper.js
@@ -3,17 +3,17 @@ const client = require('../config/db');
 const furnitureDatamapper = {
     
     async findAll() {
-            const result = await client.query('SELECT * FROM furniture');
+            const result = await client.query('SELECT furniture.name AS furniture_name, * FROM "furniture" INNER JOIN furniture_photo ON furniture_photo.furniture_id = furniture.id WHERE cover_photo = true');
             return result.rows;
     },
 
     async findByPk(id) {
         const preparedQuery = {
-            text: `SELECT * FROM "furniture" WHERE "id" = $1`,
+            text: 'SELECT furniture.name AS furniture_name,furniture_photo AS photo_name, * FROM "furniture" INNER JOIN furniture_photo ON furniture_photo.furniture_id = furniture.id WHERE furniture_id = $1',
             values: [id]
         }
         const result = await client.query(preparedQuery);
-        return result.rows[0];
+        return result.rows;
     },
 
     async delete(id) {
